4fb707830610169bb901106208b89378489f576e
cyrillee <0njic97@solent.ac.uk>
2025-10-03 14:23:00 +0100

    Testing Pycharm with GitHub.

--- if_statements.py
+++ if_statements.py
@@ -5,3 +5,11 @@ if book == "adventure":
    print("I Like adventure books!")
 
 print("finished reading the book.")
+
+
+print("please enter the activity to be performed.")
+activity=input()
+if activity == "calculations":
+    print("calculations")
+else: print("performing activity")
+print("activity completed!")
