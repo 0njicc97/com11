134174b6b4e3700bbcf133b77b99e6b88f58408f
cyrillee <0njic97@solent.ac.uk>
2025-10-03 22:55:41 +0100

    Testing Pycharm with GitHub.

--- if_statements.py
+++ if_statements.py
@@ -25,3 +25,13 @@ elif direction == "left":
 elif direction == "right":
     print("i will go right")
 
+print("please enter a whole number")
+number=int(input())
+if number % 2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
+
+
