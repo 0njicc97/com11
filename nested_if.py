47020ecb9831500f1abc39d457fd283126a10df4
cyrillee <0njic97@solent.ac.uk>
2025-10-04 14:05:56 +0100

    Testing Pycharm with GitHub

---
+++ nested_if.py
@@ -0,0 +1,15 @@
+print("enter the cover type ( hard or soft ) of the book.")
+book_type = input()
+if book_type == "soft_cover":
+    print("soft cover, perfect bounds book are very popular!")
+    print("soft covers with coils or stitches are great for short books")
+else:
+    if book_type == "hard_cover":
+        print("books with har covers can be more expensive!")
+
+
+
+
+
+
+
