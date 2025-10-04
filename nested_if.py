bf585afb8b951e93f1310924817abb73c486bfbf
cyrillee <0njic97@solent.ac.uk>
2025-10-04 14:32:41 +0100

    Testing Pycharm with GitHub

--- nested_if.py
+++ nested_if.py
@@ -8,6 +8,20 @@ else:
         print("books with har covers can be more expensive!")
 
 
+print("where should I look?")
+look=input()
+if look == "in_the_bedroom":
+    print("where in the bedroom should I look?")
+    if look == "in_the_cupboard":
+        print("found some mess but no phone.")
+elif look == "in the living room":
+    print("where in the living room should I look?")
+    if look == "on the table":
+        print("yes! found my phone")
+    else:
+        print("found some stuffs but no phone.")
+
+
 
 
 
