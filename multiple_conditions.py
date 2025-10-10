64d4b68eb25e73755d2f6b15398ec91b12dd4516
cyrillee <0njic97@solent.ac.uk>
2025-10-10 12:06:57 +0100

    Testing Pycharm with GitHub

--- multiple_conditions.py
+++ multiple_conditions.py
@@ -7,3 +7,23 @@ else:
         print("taking the safe route!")
     else:
         print("not sure which route to take.")
+
+        print("what did i hear?")
+    response = input()
+    if response == "hook":
+        print("hook hook!")
+    else:
+        print("what did i see?")
+        response = input()
+        if response == "some big eyes":
+            print("i am  little bit scared but I will continue")
+        else:
+            response = "grr and two red eyes"
+            print("there is a scary creature. I should get out of there")
+
+
+
+
+
+
+
