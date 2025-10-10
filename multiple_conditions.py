78fd0226749a9bf7df0b83cbaedfd4e779faefcf
cyrillee <0njic97@solent.ac.uk>
2025-10-10 11:30:48 +0100

    Testing Pycharm with Github

---
+++ multiple_conditions.py
@@ -0,0 +1,9 @@
+print("what type of adventure should i have?")
+adventure = input()
+if adventure == "scary or short":
+    print("entering the dark forest!")
+else:
+    if adventure == "safe or long":
+        print("taking the safe route!")
+    else:
+        print("not sure which route to take.")
