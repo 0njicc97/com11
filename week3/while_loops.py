938f8a9d93cecdefe300469dd2468dd462ef834e
cyrillee <0njic97@solent.ac.uk>
2025-10-10 14:29:11 +0100

    Testing Pycharm with github

--- week3/while_loops.py
+++ week3/while_loops.py
@@ -2,3 +2,7 @@ print("how many apples should i remove?")
 num = int(input())
 for i in range(num):
     print("Removed apple.")
+    print("how many obstacles most i avoid?")
+    num = int(input())
+    for i in range(num):
+        print("Avoiding...Done! ")
