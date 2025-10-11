8ee6edd7c2598e740f6637774c4410f625c962a4
cyrillee <0njic97@solent.ac.uk>
2025-10-11 15:15:05 +0100

    Testing Pycharm with GitHub

--- For_loops.py
+++ For_loops.py
@@ -2,3 +2,10 @@ print("howmany mountains should i display")
 response=input()
 for response in range(int(response)):
     print(response)
+
+    print("How far are we from the target?")
+    response=input() + "number of steps"
+    for response in range(int(response)):
+        print(response)
+
+
