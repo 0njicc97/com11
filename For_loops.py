f4cf996bf73411aa29c0c457bb702df0bef7561a
cyrillee <0njic97@solent.ac.uk>
2025-10-13 00:46:03 +0100

    Testing Pycharm with GitHub

--- For_loops.py
+++ For_loops.py
@@ -1,11 +1,27 @@
-print("howmany mountains should i display")
-response=input()
-for response in range(int(response)):
-    print(response)
+#print("howmany mountains should i display")
+#response=input()
+#for response in range(int(response)):
+    #print(response)
 
-    print("How far are we from the target?")
-    response=input() + "number of steps"
-    for response in range(int(response)):
-        print(response)
+    #print("How far are we from the target?")
+    #response=input() + "number of steps"
+    #for response in range(int(response)):
+        #print(response)
+       # break
+
+       print("howmany apples should I remove?.")
+       apples_to_remove=int(input())
+       remove_apples = 0
+       while remove_apples < apples_to_remove:
+           remove_apples = remove_apples + 1
+           print(remove_apples)
+
+
+
+
+
+
+
+
 
 
