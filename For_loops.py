aeaca2929c1a293a303c3f78e2319f75b095351f
cyrillee <0njic97@solent.ac.uk>
2025-10-13 01:12:31 +0100

    Testing pycharm with GitHub

--- For_loops.py
+++ For_loops.py
@@ -16,14 +16,24 @@
           # remove_apples = remove_apples + 1
           # print(remove_apples)
 
-          print("howmany obstacles most I avoid?.")
-          obstacles_to_avoid = int(input())
-          obstacles_avoided = 0
-          while obstacles_avoided < obstacles_to_avoid:
-            print("Avoiding...", end="")
+          #print("howmany obstacles most I avoid?.")
+          #obstacles_to_avoid = int(input())
+          #obstacles_avoided = 0
+          #while obstacles_avoided < obstacles_to_avoid:
+            #print("Avoiding...", end="")
 
-              obstacles_avoided += 1
-              print(f"Done {obstacles_avoided}/{obstacles_to_avoid}")
+              #obstacles_avoided += 1
+              #print(f"Done {obstacles_avoided}/{obstacles_to_avoid}")
+
+
+print("howmany bar should be charge?")
+response = input()
+bars_charged = 0:
+while response < bars_charged:
+    print("charging")
+    print("the battery is fully charged")
+
+
 
 
 
