efa381a63b03f423863b47e3de123d5912af563b
cyrillee <0njic97@solent.ac.uk>
2025-10-17 18:14:45 +0100

    Testing Pycharm with GitHub

--- week4/custom_function.py
+++ week4/custom_function.py
@@ -1,10 +1,8 @@
-def my_function():
-    print("enter a word representing sound")
-    word = input()
-    print(f"That was too loud {word}" )
-
-my_function()
-
-
-
+def my_function(identity):
+ print("enter a word representing what you see")
+word = input("enter a word: ")
+if word == "a large boulder":
+ print("is time to run")
+else:
+ print("we will be fine")
 
