85bd1a39776984feb91f976f95e89c3271142bec
cyrillee <0njic97@solent.ac.uk>
2025-10-17 14:26:01 +0100

    Testing pycharm with GitHub

---
+++ week4/build_in_function.py
@@ -0,0 +1,24 @@
+print("program started!")
+print("please enter a standard character")
+character = input()
+
+if len(character) == 1:
+    ASCII = ord(character)
+    print(f"The ASCII code for {character} is {ASCII}")
+else:
+    print("enter a single latter")
+
+print("program ended")
+
+
+
+
+
+
+
+
+
+
+
+
+
