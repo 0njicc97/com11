86dff6a9d7303709e06f56b99db9ace0b4abb6f9
cyrillee <0njic97@solent.ac.uk>
2025-10-18 01:23:54 +0100

    Testing Pycharm with Github

--- week4/custom_function.py
+++ week4/custom_function.py
@@ -1,6 +1,4 @@
 
-
-
 def escape_by ():
  plan = input("enter a plan: ")
  if plan == "jumping over":
@@ -12,3 +10,13 @@ def escape_by ():
  else: print("we cannot escape that way! The boulder is on the way")
 
  escape_by()
+
+
+def cross_bridge():
+ print("cross steps")
+cross_steps =input("how many cross steps?")
+if cross_steps > "5":
+ print("the bridge is collapsing")
+elif cross_steps < "5":
+ print("we most keep going")
+cross_bridge()
