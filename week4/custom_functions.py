ec12cd0e11890b2f4b0c088e2304404480cae325
cyrillee <0njic97@solent.ac.uk>
2025-10-22 00:19:38 +0100

    Testing Pycharm with GitHub

--- week4/custom_functions.py
+++ week4/custom_functions.py
@@ -1,8 +1,20 @@
-def listen ():
-    print("please enter a word representing a sound")
+#def listen ():
+    #print("please enter a word representing a sound")
+    #word = input()
+    #print(f"That was loud  {word}")
+#listen()
+
+
+def identity ():
+    print("please enter a word representing what you see?")
     word = input()
-    print(f"That was loud  {word}")
-listen()
+    if word == "a_large_boulder":
+        print("it is time to run!")
+    else:
+        print("we will be fine.")
+    identity()
+
+
 
 
 
