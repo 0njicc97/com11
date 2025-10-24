30098e9eb7802eccbfbce412f71067eb0a478429
cyrillee <0njic97@solent.ac.uk>
2025-10-24 14:11:07 +0100

    Testing Pycharm with github

---
+++ week 5/list_tasks.py
@@ -0,0 +1,10 @@
+def direction():
+    steps = ["move forward", "move backward", "turn left", "turn right"]
+    return steps
+
+
+def run_task1():
+    result = direction()
+    print(result)
+
+run_task1()
