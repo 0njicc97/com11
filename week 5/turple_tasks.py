d0fdee104ad20c21a91cb811ea05094b65c1e57d
cyrillee <0njic97@solent.ac.uk>
2025-10-25 00:02:18 +0100

    TESTING PYCHARM WITH GITHUB

--- week 5/turple_tasks.py
+++ week 5/turple_tasks.py
@@ -5,3 +5,14 @@ def run_task1():
     value = likelihood()
     print(f"Minimum likelihood of falling:{value}%")
 run_task1()
+
+
+def likelihood_min_max():
+    likelihoods = (50,38,27,99,4)
+    return min and max (likelihoods)
+
+def run_task2():
+    value = likelihood_min_max()
+    print(f"Minimum likelihood{value}% maximum likelihood {value:}%")
+run_task2()
+
