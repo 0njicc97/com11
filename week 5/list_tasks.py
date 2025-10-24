b580e1540a2e622d0703a42b717cb3971cec19a9
cyrillee <0njic97@solent.ac.uk>
2025-10-24 18:48:03 +0100

    Testing pycharm with github

--- week 5/list_tasks.py
+++ week 5/list_tasks.py
@@ -10,17 +10,34 @@
 #run_task1()
 
 
-def movement ():
-    path = ["move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
-    return path
+#def movement ():
+   # path = ["move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
+  #  return path
 
 
-def run_task2():
-    print("moving...")
-    directions = movement()
-    for i in range(0, len(directions), 2):
-        print(f"{directions[i]} for {directions[i+1]} steps")
+#def run_task2():
+    #print("moving...")
+    #directions = movement()
+    #for i in range(0, len(directions), 2):
+        #print(f"{directions[i]} for {directions[i+1]} steps")
 
-run_task2()
+#run_task2()
+
+def direction():
+    steps = ["move forward","move backward","Turn left","Turn right",]
+    return steps
+
+def menu():
+     print("please select a direction:")
+     steps = direction()
+     for x in range(0,len(steps)):
+       print(f"{steps[x]}")
+menu()
+
+
+
+
+
+
 
 
