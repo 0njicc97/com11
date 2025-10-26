76779e40797afdd990745aaf42468587adf658d9
cyrillee <0njic97@solent.ac.uk>
2025-10-26 23:53:23 +0000

    Testin Pycharm with GitHub

--- week1/week2/if_statements.py
+++ week1/week2/if_statements.py
@@ -3,3 +3,12 @@ book_type = input()
 if book_type == "adventure":
     print("I like adventures book!.")
     print("Finished reading book!.")
+
+print("please enter activity to be performed?")
+activity = input()
+if activity == "calculate":
+    print("performing calculations...")
+    print("activity completed")
+else:
+    print("performing activity...")
+    print("activity completed")
