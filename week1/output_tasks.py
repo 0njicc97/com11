720482e841601b5a676ffc0c5a354d0a105e4e98
cyrillee <0njic97@solent.ac.uk>
2025-10-26 18:36:16 +0000

    Testing Pycharm with GitHub

--- week1/output_tasks.py
+++ week1/output_tasks.py
@@ -1,5 +1,9 @@
-print("Welcome to com411")
+print("Welcome to com411!")
 
+print("In week 1 we will learn ...")
 
-print ("welcome to com411!")
-print("in week 1 we will learn ...")
+print("...How to use Git and GitHub")
+print("...How to output to the screen" )
+print("...How to get user input")
+
+print("I hope you are enjoying the lesson thus far!")
