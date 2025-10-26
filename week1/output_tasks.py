5459f3a6b721d72b571bec8a13ac5e6125d8899c
cyrillee <0njic97@solent.ac.uk>
2025-10-26 18:46:35 +0000

    Testing Pycharm with Github

--- week1/output_tasks.py
+++ week1/output_tasks.py
@@ -7,3 +7,11 @@ print("...How to output to the screen" )
 print("...How to get user input")
 
 print("I hope you are enjoying the lesson thus far!")
+
+print("\n Display a new new line")
+print("\t Display a tab space")
+print("\\ Display a back slash")
+print("\" Display a double quote")
+print("\" Display a single quote")
+
+print("\t I am programming!")
