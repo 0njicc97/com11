292da3315b5d146a0783c4d6ee7505f25f02fcf7
cyrillee <0njic97@solent.ac.uk>
2025-10-27 00:04:38 +0000

    Testing Pycharm with GitHub

--- week1/week2/if_statements.py
+++ week1/week2/if_statements.py
@@ -12,3 +12,17 @@ if activity == "calculate":
 else:
     print("performing activity...")
     print("activity completed")
+
+print("towards which direction should i go,(up,down,left or right)")
+direction = input()
+if direction == "up":
+    print("I will go up...")
+elif direction == "down":
+    print("I will go down...")
+elif direction == "left":
+    print("I will go left...")
+elif direction == "right":
+    print("I will go right...")
+else:
+    print("not sure which direction should i go")
+
