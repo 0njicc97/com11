4fbc8908fd4087b5f12c7eeb4fb4cf0f3d04a959
cyrillee <0njic97@solent.ac.uk>
2025-10-28 22:50:01 +0000

    Testing Pycharm with GitHub

--- week3/while_loops.py
+++ week3/while_loops.py
@@ -1,13 +1,24 @@
-print("howmany apples should I remove")
-apples_to_remove = int(input())
+# print("howmany apples should I remove")
+# apples_to_remove = int(input())
+#
+# apples_removed = 0
+#
+#
+# print()
+#
+# while apples_removed < apples_to_remove:
+#
+#     print("apples_to_remove")
+#     apples_removed = apples_to_remove + 1
+#
 
-apples_removed = 0
 
+print("Howmany obstacles should I avoid?")
+obstacles_to_avoid = int(input())
+obstacles_avoided = 0
 
-print()
-
-while apples_removed < apples_to_remove:
-    apples_to_remove = apples_to_remove - apples_removed
-    print("apples_to_remove")
-    apples_removed = apples_to_remove + 1
-
+while obstacles_avoided < obstacles_to_avoid:
+    print("Avoiding")
+    obstacles_avoided += 1
+    print(f"Avoiding...done! {obstacles_avoided} obstacles avoided")
+print("All obstacles have been avoided.")
