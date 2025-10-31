5d73505d70f4775efa1e7dae3fcfdf30d7e41ac5
cyrillee <0njic97@solent.ac.uk>
2025-10-31 14:31:45 +0000

    Testing Pycharm with Github

--- week4/week6/txt_file_tasks.py
+++ week4/week6/txt_file_tasks.py
@@ -1,11 +1,44 @@
-import os
+# import os
+#
+#
+# def cwd():
+#     path = os.getcwd()
+#
+#     print(f"current working Directory is: {path}")
+#     print("The Directory contains of the following files:")
+#     for file in os.listdir(path):
+#         print(file)
+#
+# cwd()
+
+def display_chars(file_path,num_chars):
+    with open(file_path,"r") as f:
+        under_contents = f.read(num_chars)
+        print(under_contents)
+display_chars("library.txt",8)
 
 
-def cwd():
-    path = os.getcwd()
+def display_line(file_path):
+    with open(file_path,"r") as f:
+        lines = f.readline()
+        print(lines)
+display_line("library.txt")
 
-    print(f"current working Directory is: {path}")
-    print("The Directory contains of the following files:")
-    for file in os.listdir(path):
-        print(file)
-cwd()
+
+def display_text(file_path):
+    with open(file_path,"r") as f:
+        text = f.read()
+        print(text)
+display_text("library.txt")
+
+def run_task2():
+    """run all diplay function on library text"""
+    file_path = "library.txt"
+    display_text(file_path)
+    display_chars(file_path,8)
+
+
+if __name__ == "__main__":
+    run_task2()
+
+
