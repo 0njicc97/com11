a2cd0658bcc8ebc3a5ab68f4b10f93d6a9cf47eb
cyrillee <0njic97@solent.ac.uk>
2025-11-07 14:05:19 +0000

    Testing Pycharm with GitHub

--- week7/set_tasks.py
+++ week7/set_tasks.py
@@ -1,17 +1,45 @@
-def observe():
+# def observe():
+#
+#      observations = {"car","sky scrapper","sky scrapper","bike","house","house"}
+#
+#
+#      return observations
+#
+# def run_task1():
+#
+#     items = observe()
+#
+#     print(items)
+#
+#
+#
+# run_task1()
+#
 
-     observations = {"car","sky scrapper","sky scrapper","bike","house","house"}
 
+def observed_items():
+    observations = []
+    for i in range(7):
+        user_input = input("Enter observation: ")
+        observations.append(user_input)
+    return observations
 
-     return observations
-
-def run_task1():
-
-    items = observe()
+def run_task2():
+    print("counting observations...")
+    observations = observed_items()
+    items = set()
+    for i in observations:
+        counts = (i,observations.count(i))
+        items.add(counts)
 
     print(items)
 
+run_task2()
 
 
-run_task1()
+
+
+
+
+
 
