6fbdac9b023ca101670614a216529f4b86a28af5
cyrillee <0njic97@solent.ac.uk>
2025-11-07 13:44:54 +0000

    Testing Pycharm with GitHub

--- week4/week6/txt_file_task.py
+++ week4/week6/txt_file_task.py
@@ -45,15 +45,64 @@ import csv
 #     run_task2()
 
 
-def search(filename):
-    print("searching...")
-    with open(filename) as file:
-        for location in file:
-            print(f"look in {location.strip()}")
-            print("Done!")
+# def search(filename):
+#     print("searching...")
+#     with open(filename) as file:
+#         for location in file:
+#             print(f"look in {location.strip()}")
+#             print("Done!")
+#
+# def run_task3():
+#     search("library.txt")
+#
+# if __name__ == "__main__":
+#     run_task3()
 
-def run_task3():
-    search("library.txt")
+# def search():
+#     print("searching!")
+#     sections = ""
+#     books =  "Books:\n"
+#     with open('file_path') as f:
+#         for line in f:
+#             if line.startswith(sections):
+#                 sections += line
+#             else:
+#                 books += line
+#         print("Done!")
+#         return f"{sections}\n\n{books}"
+# def save(file_path, data):
+#     print("saving!")
+#     with open(file_path, 'w') as f:
+#         f.write(data)
+#     print("Done!")
+#
+# def run_task4():
+#     data = search("books.txt")
+#     save("export_books.txt", data)
+#
+#
+# if __name__ == "__main__":
+#     run_task4()
+
+
+import csv
+
+
+def read(file_path):
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+
+        headings = next(csv_reader)
+        print(f"Headings:\n{headings}")
+
+        print("Values:")
+        for values in csv_reader:
+            print(values)
+
+
+def run_task1():
+    read("clothing.csv")
+
 
 if __name__ == "__main__":
-    run_task3()
+    run_task1()
---
+++ week7/set_tasks.py
@@ -0,0 +1,17 @@
+def observe():
+
+     observations = {"car","sky scrapper","sky scrapper","bike","house","house"}
+
+
+     return observations
+
+def run_task1():
+
+    items = observe()
+
+    print(items)
+
+
+
+run_task1()
+
