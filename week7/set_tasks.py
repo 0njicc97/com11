82dac31034dfe62f8ed7a81116ed6a828ceaa515
cyrillee <0njic97@solent.ac.uk>
2025-11-07 14:29:47 +0000

    Testing Pycharm with GitHub

--- week7/set_tasks.py
+++ week7/set_tasks.py
@@ -16,25 +16,59 @@
 # run_task1()
 #
 
+#
+# def observed_items():
+#     observations = []
+#     for i in range(7):
+#         user_input = input("Enter observation: ")
+#         observations.append(user_input)
+#     return observations
+#
+# def run_task2():
+#     print("counting observations...")
+#     observations = observed_items()
+#     items = set()
+#     for i in observations:
+#         counts = (i,observations.count(i))
+#         items.add(counts)
+#
+#     print(items)
+#
+# run_task2()
+#
+#
+#
+#
+#
 
-def observed_items():
-    observations = []
-    for i in range(7):
-        user_input = input("Enter observation: ")
-        observations.append(user_input)
-    return observations
+def observe_items():
+    observation = []
+    for i in range(5):
+        user = input("enter values")
+        observation.append(user)
+    return observation
+def remove_observations(observation):
+    print("Do you wish to remove observations?")
+    answer = input("yes/no")
+    while answer == "yes":
+        print("enter a string representing observation to be removed")
+        respond = input()
+        observation.remove(respond)
+        print("Do you wish to remove observations?")
+        answer = input("yes/no")
 
-def run_task2():
-    print("counting observations...")
-    observations = observed_items()
-    items = set()
-    for i in observations:
-        counts = (i,observations.count(i))
-        items.add(counts)
+    return observation
 
-    print(items)
+def run_task3():
+    observation = observe_items()
+    removed=remove_observations(observation)
+    print(removed)
+run_task3()
 
-run_task2()
+
+
+
+
 
 
 
