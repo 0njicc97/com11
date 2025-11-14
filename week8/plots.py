bcbdc1bd687f14c9f7f39b8f9da200713ddb8287
cyrillee <0njic97@solent.ac.uk>
2025-11-14 13:49:26 +0000

    Testing Pycharm with Github

--- week8/plots.py
+++ week8/plots.py
@@ -1,13 +1,37 @@
+# import matplotlib.pyplot as plt
+#
+#
+#
+# def display_lines(x_values,y_values):
+#     plt.plot(x_values,y_values)
+#
+# def run_task1():
+#     x_values = (1,2,3,4,5)
+#     y_values = (1,4,9,16,25)
+#     display_lines(x_values,y_values)
+#     plt.show()
+# run_task1()
+
 import matplotlib.pyplot as plt
 
+def small():
+    x = [3,4,4,3,3]
+    y = [3,3,4,4,3]
+    plt.plot(x,y, "ro--")
+
+def medium():
+    x = [2,5,5,2,2]
+    y = [2,2,5,5,2]
+    plt.plot(x,y,"gs--")
+
+def large():
+    x = [1,6,6,1,1]
+    y = [1,1,6,6,1]
+    plt.plot(x,y,"bx-")
 
 
-def display_lines(x_values,y_values):
-    plt.plot(x_values,y_values)
 
-def run_task1():
-    x_values = (1,2,3,4,5)
-    y_values = (1,4,9,16,25)
-    display_lines(x_values,y_values)
-    plt.show()
-run_task1()
+small()
+medium()
+large()
+plt.show()
