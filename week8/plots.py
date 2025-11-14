3558f34350f6bb646e9168f9246715c1c53c8db4
cyrillee <0njic97@solent.ac.uk>
2025-11-14 14:34:33 +0000

    Testing Pycharm with GitHub

--- week8/plots.py
+++ week8/plots.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-#
+import random
 #
 #
 # def display_lines(x_values,y_values):
@@ -36,28 +36,62 @@ import matplotlib.pyplot as plt
 # large()
 #plt.show()
 
-def coordinate():
-    print("please enter and x_values")
-    x_values = input()
-    print("please enter and y_values")
-    y_values = input()
-    return x_values, y_values
+# def coordinate():
+#     print("please enter and x_values")
+#     x_values = input()
+#     print("please enter and y_values")
+#     y_values = input()
+#     return x_values, y_values
+#
+# def path():
+#     print("retrieving path..")
+#     x_values = []
+#     y_values = []
+#     for i in range(4):
+#         data = coordinate()
+#         x,y = data
+#         x_values.append(x)
+#         y_values.append(y)
+#     return [x_values, y_values]
+# def run_task3():
+#     values = path()
+#     plt.plot(values[0], values[1], 'r--o')
+#     plt.show()
+# run_task3()
+#
 
-def path():
-    print("retrieving path..")
-    x_values = []
-    y_values = []
-    for i in range(4):
-        data = coordinate()
-        x,y = data
-        x_values.append(x)
-        y_values.append(y)
-    return [x_values, y_values]
-def run_task3():
-    values = path()
-    plt.plot(values[0], values[1], 'r--o')
+
+def data():
+    path = {}
+    print("What kind of line you will like (-or--) ?")
+    line = input()
+    print("What colour you will like (r,g or b) ?")
+    colour = input()
+    print("What style of marker you will like (o,s or ) ?")
+    marker = input()
+
+    path["line"] = line
+    path["colour"] = colour
+    path["marker"] = marker
+
+    return path
+
+def generate():
+
+    print("how many lines you will you like to generate ?")
+    lines = int(input())
+
+    for i in range(lines):
+
+        values = data()
+        x = random.sample(range(1, 51), 5)
+        y = random.sample(range(1, 51), 5)
+        plt.plot(x,y,f'{values["line"]}{values["colour"]}{values["marker"]}')
     plt.show()
-run_task3()
+
+generate()
+
+
 
 
 
