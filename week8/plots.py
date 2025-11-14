8a34765da54fb1fda9fa0a896b4396210444da54
cyrillee <0njic97@solent.ac.uk>
2025-11-14 14:13:48 +0000

    Testing Pycharm with github

--- week8/plots.py
+++ week8/plots.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #
 #
 #
@@ -12,26 +12,56 @@
 #     plt.show()
 # run_task1()
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+#
+# def small():
+#     x = [3,4,4,3,3]
+#     y = [3,3,4,4,3]
+#     plt.plot(x,y, "ro--")
+#
+# def medium():
+#     x = [2,5,5,2,2]
+#     y = [2,2,5,5,2]
+#     plt.plot(x,y,"gs--")
+#
+# def large():
+#     x = [1,6,6,1,1]
+#     y = [1,1,6,6,1]
+#     plt.plot(x,y,"bx-")
+#
+#
+#
+# small()
+# medium()
+# large()
+#plt.show()
 
-def small():
-    x = [3,4,4,3,3]
-    y = [3,3,4,4,3]
-    plt.plot(x,y, "ro--")
+def coordinate():
+    print("please enter and x_values")
+    x_values = input()
+    print("please enter and y_values")
+    y_values = input()
+    return x_values, y_values
 
-def medium():
-    x = [2,5,5,2,2]
-    y = [2,2,5,5,2]
-    plt.plot(x,y,"gs--")
+def path():
+    print("retrieving path..")
+    x_values = []
+    y_values = []
+    for i in range(4):
+        data = coordinate()
+        x,y = data
+        x_values.append(x)
+        y_values.append(y)
+    return [x_values, y_values]
+def run_task3():
+    values = path()
+    plt.plot(values[0], values[1], 'r--o')
+    plt.show()
+run_task3()
 
-def large():
-    x = [1,6,6,1,1]
-    y = [1,1,6,6,1]
-    plt.plot(x,y,"bx-")
 
 
 
-small()
-medium()
-large()
-plt.show()
+
+
+
